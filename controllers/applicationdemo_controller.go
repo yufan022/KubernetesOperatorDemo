ab34c953d862719b8c5a4b2a24dd0b17c2fd18d5
wangyufan <wangyufan@chainup.com>
2020-03-27 20:42:26 +0800

    test

--- controllers/applicationdemo_controller.go
+++ controllers/applicationdemo_controller.go
@@ -20,8 +20,6 @@ import (
 	"fmt"
 
 	"github.com/go-logr/logr"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
